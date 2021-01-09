0280c101b4feb9a51cd19f093e07d56e67076c27
FuGuiJar <fuguijar@qq.com>
2021-01-09 18:09:30 +0800

    -v1.2

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 window.onload = function(){
-document.getElementsByTagName("head")[0].append();
-
-
+document.getElementsByTagName("head")[0].append('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/FuGuiJar/A/lib/A.css">');
 }
